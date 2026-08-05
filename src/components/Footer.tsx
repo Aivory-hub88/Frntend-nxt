769c7f8a3d6ee8b942e5b652c08574e12ae4c9f4
Aivory-hub88 <irfan.reichmann@aivory.id>
2026-08-05 10:04:33 +0700

    Revert "feat(footer): add NVIDIA Inception Program badge below Aivory logo"
    This reverts commit 55c0cae5274804dbb3d1da83168e0bee8463a8ec.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -94,16 +94,11 @@ export default function Footer() {
 
             {/* Logo */}
             <div className="col-span-2 md:col-span-1 flex md:justify-end mt-8 md:mt-0">
-              <div className="flex flex-col items-start md:items-end gap-4">
+              <div className="flex flex-col items-start">
                 <img
                   src="/Aivory_logo_2_2026.svg"
                   alt="Aivory Logo"
-                  className="h-[36px] md:h-[52px] w-auto opacity-90"
-                />
-                <img
-                  src="/images/Nvidia Inception/nvidia-inception-program-badge-rgb-for-screen.svg"
-                  alt="NVIDIA Inception Program"
-                  className="h-[40px] md:h-[56px] w-auto"
+                  className="h-[48px] md:h-[72px] w-auto opacity-90"
                 />
               </div>
             </div>
